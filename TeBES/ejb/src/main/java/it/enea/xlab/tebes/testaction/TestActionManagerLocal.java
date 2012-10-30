cc794266b10ab52b582ec4430fffda75de6f0778
cristiano.novelli@gmail.com <cristiano.novelli@gmail.com@3fa942bf-d5ce-4844-cb62-2250a1f73969>
2012-10-30 09:07:31 +0000

    Creazione di TestPlanManagerImplITCase.

--- TeBES/ejb/src/main/java/it/enea/xlab/tebes/testaction/TestActionManagerLocal.java
+++ TeBES/ejb/src/main/java/it/enea/xlab/tebes/testaction/TestActionManagerLocal.java
@@ -3,12 +3,12 @@ package it.enea.xlab.tebes.testaction;
 import javax.ejb.Local;
 
 import it.enea.xlab.tebes.model.Action;
-import it.enea.xlab.tebes.model.TestPlan;
+import it.enea.xlab.tebes.model.TestPlanOLD;
 
 @Local
 public interface TestActionManagerLocal {
 
 	public boolean execute(Action action);
 	
-	public boolean executeActionWorkflow(TestPlan testPlan);
+	public boolean executeActionWorkflow(TestPlanOLD testPlan);
 }
--- TeBES/ejb/src/main/java/it/enea/xlab/tebes/testplan/TestPlanManagerLocal.java
+++ TeBES/ejb/src/main/java/it/enea/xlab/tebes/testplan/TestPlanManagerLocal.java
@@ -1,13 +1,16 @@
 package it.enea.xlab.tebes.testplan;
 
-import it.enea.xlab.tebes.model.TestPlan;
+import it.enea.xlab.tebes.entity.TestPlan;
+import it.enea.xlab.tebes.model.TestPlanOLD;
 
 import javax.ejb.Local;
 
 @Local
 public interface TestPlanManagerLocal {
 
-	public TestPlan importTestPlan(String testPlanFilePath, String userId);
+	public TestPlanOLD importTestPlan(String testPlanFilePath, String userId);
 	
 	public TestPlanDOM getTestPlanDOM();
+	
+	public Long createTestPlan(TestPlan testPlan);
 }
