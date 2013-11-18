15a0aafd31fcbe14330de46255367ebfb8f4462a
fulvio.dimarco <fulvio.dimarco@3fa942bf-d5ce-4844-cb62-2250a1f73969>
2013-11-18 13:24:04 +0000

    Selettore lingua e framework cambio lingua

---
+++ trunk/TeBES/war/src/main/java/it/enea/xlab/tebes/controllers/localization/LocalizationController.java
@@ -0,0 +1,33 @@
+package it.enea.xlab.tebes.controllers.localization;
+
+import java.util.Locale;
+
+import javax.faces.context.FacesContext;
+
+public class LocalizationController {
+
+	private static String locale = "it";
+
+	public void setLocale(String newLocale) {
+		LocalizationController.locale = newLocale;
+	}
+
+	public synchronized String getLocale() {
+		return locale;
+	}
+
+	public synchronized String italianLocalization() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getViewRoot().setLocale(Locale.ITALIAN);
+		this.setLocale("it");
+		return "changed";
+	}
+
+	public synchronized String englishLocalization() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getViewRoot().setLocale(Locale.ENGLISH);
+		this.setLocale("en");
+		return "changed";
+	}
+
+}
