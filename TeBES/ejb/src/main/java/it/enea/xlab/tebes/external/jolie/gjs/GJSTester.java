585a21f5712685350aab6442cdc2fde640c0d5ef
cristiano.novelli@gmail.com <cristiano.novelli@gmail.com@3fa942bf-d5ce-4844-cb62-2250a1f73969>
2014-09-19 17:44:12 +0000

    Test OK. Work on GJS integration in progress...

--- TeBES/ejb/src/main/java/it/enea/xlab/tebes/external/jolie/gjs/GJSTester.java
+++ TeBES/ejb/src/main/java/it/enea/xlab/tebes/external/jolie/gjs/GJSTester.java
@@ -2,7 +2,8 @@ package it.enea.xlab.tebes.external.jolie.gjs;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Hashtable;
+
+import jolie.runtime.Value;
 
 public class GJSTester {
 
@@ -13,50 +14,99 @@ public class GJSTester {
 	 */
 	public static void main(String[] args) throws Exception {
 		
-		/*
-		String[][] parameters = new String[2][2];
-		
-		parameters[0][0] = "CountryName";
-		parameters[0][1] = "Italy";
-
-		parameters[1][0] = "CityName";
-		parameters[1][1] = "Bologna";
-
-		GJS.generateClientWS("http://www.webservicex.net/globalweather.asmx?WSDL", 
-					"GetWeather", 
-					"GlobalWeatherSoap",
-					"test",
-					"./test/tmppath",
-					"report.xml",
-					"standard",
-					parameters);*/
+		System.out.println("******************************");
+		System.out.println("** GlobalWeather DoubleTest **");
+		System.out.println("******************************");
+		System.out.println();
 		
 		
-		String[][] parameters = new String[2][2];
+		// Preparo Risposta per il Server
+		String[][] parameters = new String[2][2];	
+		parameters[0][0] = "Result";
+		parameters[0][1] = "ResponseFromGlobalWeather";
 		
-		parameters[0][0] = "result";
-		parameters[0][1] = "false";
-		
-		System.out.println("start");
-		
+		// GJSResult
 		GJSResult myHandler = new GJSResult();
+		
 		try {
+			System.out.println("** Test1 GJSTester-Server **");
+			System.out.println("CALL: generateServerWS");
 			GJS.generateServerWS("http://www.webservicex.net/globalweather.asmx?WSDL", 
 					"GetWeather", 
 					"GlobalWeatherSoap",
-					"test",
-					"./test/tmppath3",
+					"GJSTester-Server",
+					"C:/Temp/server",
 					"report.xml",
 					"standard",
 					10000,
-					parameters, myHandler);
+					parameters, 
+					myHandler);
 		} catch (Exception e) {
 			myHandler.setError(e.getMessage());
 		}
 		
-		
-		System.out.println("end: " + myHandler.getDescription());
+	int i=0;
+	System.out.print("waiting for test1:");
+		while( i<3 ) {
+			try {
 
+				Thread.sleep(1000);
+				System.out.print(" " + ++i);
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println();
+		System.out.println("End GJSTester-Server: " + myHandler.getDescription());		
+		
+		System.out.println("****************************");
+		System.out.println();
+		
+		
+		String[][] parameters2 = new String[2][2];
+		
+		parameters2[0][0] = "CountryName";
+		parameters2[0][1] = "Italy";
+
+		parameters2[1][0] = "CityName";
+		parameters2[1][1] = "Bologna";
+
+		GJSResult myHandler2 = new GJSResult();
+		
+		System.out.println("** Test2 GJSTester-Client **");
+		System.out.println("CALL: generateClientWS");
+		System.out.println("Test Client for WS: " + myHandler.getUrl());
+		GJS.generateClientWS(myHandler.getUrl(), 
+					"GetWeather", 
+					"GlobalWeatherSoap",
+					"GJSTester-Client",
+					"C:/Temp/client",
+					"report.xml",
+					"standard",
+					parameters2, 
+					myHandler2);
+	
+		i=0;
+		System.out.print("waiting for test2:");
+		while( i<3 ) {
+			try {
+
+				Thread.sleep(1000);
+				System.out.print(" " + ++i);
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println();
+		
+		System.out.println("END GJSTester-Client: " + myHandler2.getDescription());		
+		System.out.println("END GJSTester-Client: " + myHandler2.getType());	
+
+		System.out.println();
+		System.out.println("******************************");
+		System.out.println();
 	}
 
 }
